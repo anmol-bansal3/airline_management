b98668e922cbe7f6698e4f84620d662be8043ae0
Anmol <bansalanmol41.ab@gmail.com>
2020-11-01 07:14:08 +0530

    change front.fxml ui and changing function

--- loginForm/src/application/MainController.java
+++ loginForm/src/application/MainController.java
@@ -8,23 +8,22 @@ import java.util.ResourceBundle;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import java.awt.event.*;  
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
-import javafx.scene.control.ComboBox;
-import javafx.scene.control.DatePicker;
-import javafx.scene.control.Label;
-import javafx.scene.control.TextField;
+import javafx.scene.control.*;
 import javafx.scene.layout.AnchorPane;
 import javafx.stage.Stage;
 import javafx.scene.Node;
+
 public class MainController implements Initializable{
 	@FXML
 	public AnchorPane rootPane;
 	@FXML
-	public Label testLabel1,testLabel2,testLabel3;
+	public Label testLabel1,testLabel2,testLabel3,testLabel4;
 	@FXML
 	public ComboBox<String> fromLocation;
 	ObservableList<String> fromList = FXCollections.observableArrayList("Mohali","Delhi","Mumbai");
@@ -33,7 +32,11 @@ public class MainController implements Initializable{
 	public ComboBox<String> toLocation;
 	ObservableList<String> toList = FXCollections.observableArrayList("Banaglore","Pune","Kolkata");
 	@FXML
-	private DatePicker dp;
+	private DatePicker dp1;
+	@FXML
+	private DatePicker dp2;
+	@FXML
+	public CheckBox check1; 
 	@Override
 	public void initialize(URL location, ResourceBundle resources) {
 		fromLocation.setItems(fromList);
@@ -47,19 +50,23 @@ public class MainController implements Initializable{
 	{
 		testLabel2.setText(toLocation.getValue());
 	}
-	public void showDate(ActionEvent event)
+	public void DepartureDate(ActionEvent event)
 	{
-		LocalDate ld = dp.getValue();
+		LocalDate ld = dp1.getValue();
 		testLabel3.setText(ld.toString());	
 	}
-	@FXML
+	public void ReturnDate(ActionEvent event)
+	{
+		LocalDate ld = dp2.getValue();
+		testLabel4.setText(ld.toString());	
+	}
 	public void loadLogin(ActionEvent event) throws Exception
 	{
 		URL url = Paths.get("../airline_management/loginForm/src/Login.fxml").toUri().toURL();
 		AnchorPane pane = FXMLLoader.load(url);
 		rootPane.getChildren().setAll(pane);
+	}
+	public void one_way(MouseEvent event) throws Exception{
 		
 	}
-	
-	
 }
